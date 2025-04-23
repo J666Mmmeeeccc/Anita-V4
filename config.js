3df5f36e4dfd661fe94962db1301c4de2d8a8b43
J666Mmmeeeccc <114634965+J666Mmmeeeccc@users.noreply.github.com>
2025-04-23 03:26:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 require('dotenv').config(); 
 
-global.owner = process.env.OWNER_NUMBER; 
+global.owner = process.env.OWNER_NUMBER; "254717400829 "
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.ownername = process.env.OWNER_NAME || "Prince Programmer ";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
+global.channeljid = process.env.CHANNEL_JID || "";
 global.autoTyping = process.env.AUTO_TYPING === 'true'; 
 global.autoRecord = process.env.AUTO_RECORD === 'true';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
